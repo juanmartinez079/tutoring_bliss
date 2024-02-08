f347e310672f6c652d349df0b428d8d196e3b137
juanmartinez079 <158757104+juanmartinez079@users.noreply.github.com>
2024-02-07 21:49:41 -0500

    Working Payroll Generator

--- generate_tutor_payroll_report.py
+++ generate_tutor_payroll_report.py
@@ -1,5 +1,8 @@
-import re
 from bs4 import BeautifulSoup
+from datetime import datetime
+import json
+import glob
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,118 +12,7 @@ import time
 import os
 
 
-def generate_report(start_date, end_date):
-    url = "https://app.tutorbird.com/Teacher/v2/en/home"
-    # set up autonomous browser and configure base driver
-    options = Options()
-    options.headless = True
-
-    driver = webdriver.Safari(options=options)
-    driver.get(url)
-
-    # find the HTML elements that relate to log in info
-    username_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "ctl00$ctl00$MainContent$contentBody$textboxEmail")))
-    password_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "ctl00$ctl00$MainContent$contentBody$textboxPassword")))
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, 'ctl00$ctl00$MainContent$contentBody$buttonLogin')))
-
-    # Input your credentials
-    username_input.send_keys('crypworld2000@gmail.com')
-    password_input.send_keys('qwertQWERT1')
-
-    submit_button.click()
-    # wait for login to proceed, change if necessary
-    time.sleep(5)
-
-    # first, navigate to the business report section
-    business_report_link = "https://app.tutorbird.com/Teacher/v2/en/reports/add"
-    driver.get(business_report_link)
-    time.sleep(2)
-
-    # find the first dropdown to select report type and click
-    custom_dropdown1 = driver.find_element(By.ID, 'mat-select-value-1')
-    custom_dropdown1.click()
-    time.sleep(2)
-
-    # find the payroll option and click
-    payroll = driver.find_element(By.ID, 'mat-option-20')
-    payroll.click()
-    time.sleep(2)
-    # select the start date calendar
-    calendar_start = driver.find_element(By.ID, "mat-input-0")
-    calendar_start.click()
-    time.sleep(1)
-
-    # first, check to see if year is in the current year
-    # then check if month is in current month
-
-    # first configure stat/end date aria labels
-    start = "[aria-label='" + start_date + "']"
-    end = "[aria-label='" + end_date + "']"
-
-    # select the first date
-    first_date = driver.find_element(By.CSS_SELECTOR, start)
-    first_date.click()
-    report_progress("selected first date")
-    time.sleep(1)
-
-    # select the end date calendar
-    calendar_start = driver.find_element(By.ID, "mat-input-1")
-    calendar_start.click()
-    report_progress("selected end date")
-    time.sleep(1)
-    # select the end date
-    last_date = driver.find_element(By.CSS_SELECTOR, end)
-    last_date.click()
-    time.sleep(1)
-
-    # select the total hours
-    # select the payroll details
-    total_hours_label = driver.find_element(By.XPATH, "//label[@for='mat-checkbox-4-input']")
-    total_hours_label.click()
-    payroll_label = driver.find_element(By.XPATH, "//label[@for='mat-checkbox-5-input']")
-    payroll_label.click()
-
-    # select report format
-    csv_report_label = driver.find_element(By.XPATH, "//label[@for='radioOptionID-mat-radio-2-input']")
-    csv_report_label.click()
-    time.sleep(0.5)
-
-    # submit the form
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//lib-legacy-button[@label="Create"]/div/button'))
-    )
-    submit_button.click()
-    report_progress("submited report")
-    # wait for report to be created
-    time.sleep(8)
-
-    # select the top option
-    top_checkbox = driver.find_element(By.ID, 'checkboxID-selectCheckBox0')
-    report_progress("got here")
-    top_checkbox.click()
-    time.sleep(1)
-
-    # click options tab
-    options = driver.find_element(By.ID, 'TeacherPortalInstructorsAdminsToolsMenuButton')
-    options.click()
-    time.sleep(3)
-
-    # click download
-    download_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//lib-legacy-menu-item[@label="Download"]/div'))
-    )
-    download_button.click()
-
-    wait_for_download()
-
-    driver.quit()
-
-
-def wait_for_download(timeout=60):
-
+def wait_for_download(timeout=20):
     start_time = time.time()
     downloads_dir = os.path.expanduser('~/Downloads')
     initial_files = set(os.listdir(downloads_dir))
@@ -137,32 +29,234 @@ def wait_for_download(timeout=60):
     return False
 
 
-def parse_HTML(script, keywords):
-    pattern = r"<(.*?)>"
-    str_html = str(script)
-    substrings = re.findall(pattern, str_html)
-    output = []
-    for i, line in enumerate(substrings):
-        if keywords in line:
-            output.append(line)
-    for line in output:
-        print("<", line, ">")
-
-
 def report_progress(message):
     print(message)
 
 
-def get_current_html(driver):
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-    parse_HTML(soup, 'Download')
-    print(soup)
+class PayrollReportGenerator:
+    def __init__(self, start, end):
+        self.start_date = start
+        self.end_date = end
+        with open('constant_variables.json', 'r') as json_file:
+            self.tutoring_constants = json.load(json_file)
+
+        self.home_address = self.tutoring_constants["home_address"]
+        self.business_report_address = self.tutoring_constants["business_report_address"]
+        self.default_save_location = self.tutoring_constants["default_download_location"]
+
+        # set up autonomous browser and configure base driver
+        options = Options()
+        options.headless = True
+
+        self.driver = webdriver.Safari(options=options)
+        self.driver.maximize_window()
+
+    def run(self):
+        self.generate_report()
+        time.sleep(5)
+        self.rename_file()
+
+    def generate_report(self):
+        # first go home screen
+        self.driver.get(self.home_address)
+        # find the HTML elements that relate to log in info
+        username_input = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.NAME, "ctl00$ctl00$MainContent$contentBody$textboxEmail")))
+        password_input = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.NAME, "ctl00$ctl00$MainContent$contentBody$textboxPassword")))
+        submit_button = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'ctl00$ctl00$MainContent$contentBody$buttonLogin')))
+
+        # Input your credentials and click login
+        username_input.send_keys('crypworld2000@gmail.com')
+        password_input.send_keys('qwertQWERT1')
+        submit_button.click()
+
+        # wait for login to proceed, change if necessary
+        time.sleep(5)
+
+        # first, navigate to the business report section
+        self.driver.get(self.business_report_address)
+        time.sleep(2)
+
+        # find the first dropdown to select report type and click
+        custom_dropdown1 = self.driver.find_element(By.ID, 'mat-select-value-1')
+        custom_dropdown1.click()
+        time.sleep(2)
+
+        # find the payroll option and click
+        payroll = self.driver.find_element(By.ID, 'mat-option-20')
+        payroll.click()
+        time.sleep(2)
+        # select the start date calendar
+        calendar_start = self.driver.find_element(By.ID, "mat-input-0")
+        calendar_start.click()
+        time.sleep(1)
+        self.get_current_html()
+
+        # first, check to see if year is in the current year
+        # then check if month is in current month
+
+        # first configure stat/end date aria labels
+        start = "[aria-label='" + self.start_date + "']"
+        end = "[aria-label='" + self.end_date + "']"
+
+        # first check to see if the dates appear on the default calendar or if we need to switch tabs
+        self.switch_to_correct_date(start)
+        # select the first date
+        first_date = self.driver.find_element(By.CSS_SELECTOR, start)
+        first_date.click()
+        report_progress("selected first date")
+        time.sleep(1)
+
+        # select the end date calendar
+        calendar_end = self.driver.find_element(By.ID, "mat-input-1")
+        calendar_end.click()
+        report_progress("selected end date")
+        time.sleep(1)
+
+        self.switch_to_correct_date(end)
+        # select the end date
+        last_date = self.driver.find_element(By.CSS_SELECTOR, end)
+        last_date.click()
+        time.sleep(1)
+
+        # select the total hours
+        # select the payroll details
+        total_hours_label = self.driver.find_element(By.XPATH, "//label[@for='mat-checkbox-4-input']")
+        total_hours_label.click()
+        payroll_label = self.driver.find_element(By.XPATH, "//label[@for='mat-checkbox-5-input']")
+        payroll_label.click()
+
+        # select report format
+        csv_report_label = self.driver.find_element(By.XPATH, "//label[@for='radioOptionID-mat-radio-2-input']")
+        csv_report_label.click()
+        time.sleep(0.5)
+
+        # submit the form
+        submit_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//lib-legacy-button[@label="Create"]/div/button'))
+        )
+        submit_button.click()
+        report_progress("submited report")
+        # wait for report to be created
+        time.sleep(8)
+
+        # select the top option
+        top_checkbox = self.driver.find_element(By.ID, 'checkboxID-selectCheckBox0')
+        top_checkbox.click()
+        time.sleep(1)
+
+        # click options tab
+        options = self.driver.find_element(By.ID, 'TeacherPortalInstructorsAdminsToolsMenuButton')
+        options.click()
+        time.sleep(3)
+
+        download_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//div[p/text()="Download"]'))
+        )
+        download_button.click()
+        wait_for_download()
+
+        self.driver.quit()
+
+    def get_current_html(self):
+        page_source = self.driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        print(soup.prettify())
+        # parse_HTML(soup, 'Download')
+
+    def rename_file(self):
+        os.chdir(self.default_save_location)
+
+        files = glob.glob("*")
+        # Sort files by modification time to get the most recent one
+        latest_file = max(files, key=os.path.getmtime)
+
+        # Rename the most recent file
+        new_name = f"tutor_payroll_{self.start_date}_{self.end_date}.csv"  # Provide your desired new name here
+        os.rename(latest_file, new_name)
+
+    def rename_file(self):
+        # Specify the directory where the files are located
+        directory = self.default_save_location
+
+        # Change to the directory if needed
+        # os.chdir(directory)
+
+        # Get a list of all files in the directory
+        files = glob.glob(os.path.join(directory, "*"))
+
+        if not files:
+            print("No files found in the directory.")
+            return
+
+        # Sort files by modification time to get the most recent one
+        latest_file = max(files, key=os.path.getmtime)
+
+        # Extract the file name
+        file_name = os.path.basename(latest_file)
+
+        # Provide your desired new name here
+        new_name = f"tutor_payroll_{self.start_date.replace('/', '_')} to {self.end_date.replace('/', '_')}.csv"
+
+        # Rename the most recent file
+        os.rename(latest_file, os.path.join(directory, new_name))
+
+        print(f"File '{file_name}' has been renamed to '{new_name}'.")
+
+    def switch_to_correct_date(self, date):
+        # Regular expression pattern to match the date
+        pattern = r"\d+/\d+/\d+"
+
+        # Find all matches of the pattern in the input string
+        matches = re.findall(pattern, date)
+
+        # If there are matches, extract the first one
+        if matches:
+            date = matches[0]
+            print("Extracted date:", date)
+        else:
+            print("No date found in the input string")
+        date = datetime.strptime(date, "%m/%d/%Y")
+
+        # Get the current month and year
+        current_month = datetime.now().month
+        current_year = datetime.now().year
+        next_year_button = self.driver.find_element(By.ID, "iconButtonID-datePickerHeaderNextYearButton")
+        last_year_button = self.driver.find_element(By.ID, "iconButtonID-datePickerHeaderPreviousYearButton")
+        next_month_button = self.driver.find_element(By.ID, "iconButtonID-datePickerHeaderNextMonthButton")
+        last_month_button = self.driver.find_element(By.ID, "iconButtonID-datePickerHeaderPreviousMonthButton")
+
+        if date.year != current_year:
+            diff = current_year - date.year
+            print("difference in years: ", diff)
+            # in the case that the input year is in the future
+            if diff < 0:
+                for i in range(abs(diff)):
+                    next_year_button.click()
+                    time.sleep(1)
+            else:
+                for i in range(abs(diff)):
+                    last_year_button.click()
+                    time.sleep(1)
+
+        if date.month != current_month:
+            diff = current_month - date.month
+            print("difference in months: ", diff)
+            # in the case that the input year is in the future
+            if diff < 0:
+                for i in range(abs(diff)):
+                    next_month_button.click()
+                    time.sleep(1)
+            else:
+                for i in range(abs(diff)):
+                    last_month_button.click()
+                    time.sleep(1)
 
 
 if __name__ == "__main__":
-    # start_date = input("Enter start date: ")
-    # end_date = input("Enter end date: ")
-    start_date = '2/2/2024'
-    end_date = '2/14/2024'
-    generate_report(start_date, end_date)
+    start_date = '3/4/2021'
+    end_date = '4/17/2024'
+    generator = PayrollReportGenerator(start_date, end_date)
+    generator.run()
--- parse_tutor_payment_details.py
+++ parse_tutor_payment_details.py
@@ -1,12 +1,59 @@
-import numpy as np
-import pandas
-import os
 import csv
-def generate_totals():
-    return
+import json
 
-def visualize_percentages():
-    
 
-def open_file():
-    return
+class TutorPayrollParser:
+    def __init__(self, doc):
+        self.doc = doc
+
+        with open("constant_variables.json", "r") as json_file:
+            self.tutoring_constants = json.load(json_file)
+
+        self.tutor_list = self.tutoring_constants["active_tutors"]
+        self.student_list = self.tutoring_constants["active_students"]
+
+    def run(self):
+        total, attendance, payments = self.generate_totals()
+        self.visualize_percentages(total, attendance, payments)
+
+    def generate_totals(self):
+        total_payment_due = 0
+        tutor_payments = {}
+        student_attendance = {}
+        current_student = ""
+        with open(self.doc, 'r') as payroll_file:
+            csv_reader = csv.reader(payroll_file)
+            for row in csv_reader:
+                print(row)
+                tutor = [name for name in self.tutor_list if name in row]
+                student = [name for name in self.student_list if name in row[2]]
+                balance_row = "Tutor Balance" in row
+                # if it is a tutor row
+                if not tutor == []:
+                    current_student = tutor[0]
+
+                # if it is a balance row
+                if balance_row:
+                    balance = row[-1]
+                    balance = float(balance[1:])
+                    tutor_payments.setdefault(current_student, balance)
+
+                # if it is a row with any other info
+                if not student == []:
+                    student = student[0]
+                    if student in student_attendance:
+                        student_attendance[student] += 1
+                    else:
+                        student_attendance.setdefault(student, 1)
+
+            total_payment_due = sum(tutor_payments.values())
+            return total_payment_due, student_attendance, tutor_payments
+
+    def visualize_percentages(self, total, attendance, payments):
+        return
+
+
+if __name__ == "__main__":
+    doc = "/Users/juanmartinez/Downloads/payroll_example.csv"
+    parser = TutorPayrollParser(doc)
+    parser.run()
